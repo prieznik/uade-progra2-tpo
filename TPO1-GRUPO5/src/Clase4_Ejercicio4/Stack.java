f570fa9548179394243b841e147454f8a3500e38
prieznik <paloma.rieznik.it@gmail.com>
2025-09-16 19:55:01 -0300

    palo:

---
+++ TPO1-GRUPO5/src/Clase4_Ejercicio4/Stack.java
@@ -0,0 +1,10 @@
+package Clase3_Ejercicio7;
+
+public interface Stack<E> {
+    public void push(E item);
+    //Quita y muestra el elemento en el tope de la pila. Si la pila está vacía, devuelve null.
+    public E pop();
+    public E top();
+    public boolean isEmpty();
+    public int size();
+}
