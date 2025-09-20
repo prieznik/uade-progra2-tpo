7a63f0d09b086c784665f4237a257b952d021e09
Agustin <agustingonzaleztam@gmail.com>
2025-09-20 17:21:02 -0300

    Exc 4 main with comments

--- TPO1-GRUPO5/src/Clase4_Ejercicio4/Main.java
+++ TPO1-GRUPO5/src/Clase4_Ejercicio4/Main.java
@@ -2,5 +2,25 @@ package Clase4_Ejercicio4;
 
 public class Main {
     public static void main(String[] args) {
+
+        /*  Implemente un método que reciba dos pilas de colas de enteros P1 y P2,
+            y retorne una nueva pila de colas Pout que sea la unión de los elementos de P1 y P2.
+
+            Cabe aclarar que los elementos de P1 y P2 están ordenados de menor a mayor en función de su tamaño,
+            y que la pila Pout debe quedar ordenada del mismo modo.
+
+                // P1 -> Pila de colas. Ej: [ [1,2,3], [4,5,6,7] ]
+                // P2 -> Pila de colas. Ej: [ [0], [8,9,10,11,12] ]
+
+                // PoutAux -> [  [8,9,10,11,12], [4,5,6,7], [1,2,3], [0] ];
+                // Pout -> [ [0], [1,2,3], [4,5,6,7], [8,9,10,11,12] ]
+
+            En el tope de las pilas se encuentra el elemento de mayor tamaño.
+            Resuelva el problema planteado exclusivamente en términos de las operaciones de los TDA Pila y TDACola.
+
+            Implementar un metodo que reciba P1 y P2 -> retorne Pout
+            Pout es la union de ambas. Ordenada de menor a mayor.
+         */
+
     }
 }
